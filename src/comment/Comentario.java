2a5d834c5f39f93623c265a0ba67571c61ed928c
Bruna <brunastefanie@alu.ufc.br>
2022-06-11 11:05:06 -0300

    Classe Comentario

---
+++ src/comment/Comentario.java
@@ -0,0 +1,34 @@
+package comment;
+public class Comentario{
+    private String conteudo;
+    private String autor;
+    private Int idAutor;
+    private String data;
+
+        public String getConteudo(){
+            return conteudo;
+        }
+        public void setConteudo(String conteudo){
+            this.conteudo = conteudo;
+        }
+        public String getAutor(){
+            return autor;
+        }
+        public void setAutor(String autor){
+            this.autor = autor;
+        }
+        public Int getIdAutor(){
+            return idAutor;
+        }
+        public void setIdAutor(Int idAutor){
+            this.idAutor = idAutor;
+        }
+        public String getData(){
+            return data;
+        }
+        public void setData(String data){
+            this.data = data;
+        }
+
+
+}
