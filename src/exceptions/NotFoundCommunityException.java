2a578ae572332c449e6473617fa2a46da655fdb8
wildneifrank <95833269+wildneifrank@users.noreply.github.com>
2022-06-26 14:00:03 -0300

    Add files via upload

---
+++ src/exceptions/NotFoundCommunityException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class NotFoundCommunityException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String getMessage(){
+		return ("Comunidade não encontrada.");
+	}
+}
