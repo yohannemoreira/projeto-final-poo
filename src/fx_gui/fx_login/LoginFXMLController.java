ff51007f3ef1c40387c6f312f6a99f9649786455
yohannemoreira <yohannemoreira@alu.ufc.br>
2022-07-09 18:27:13 -0300

    Update GUI Login

--- src/fx_gui/fx_login/LoginFXMLController.java
+++ src/fx_gui/fx_login/LoginFXMLController.java
@@ -2,10 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package fx_gui;
+package fx_gui.fx_login;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -15,9 +17,11 @@ import javafx.fxml.Initializable;
  */
 public class LoginFXMLController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    void addAccountAction(ActionEvent event) {
+    
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 	// TODO
