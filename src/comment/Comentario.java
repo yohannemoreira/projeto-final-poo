4fb29fea48959cfc9ce9bebcc67841a66418cd0c
Bruna <brunastefanie@alu.ufc.br>
2022-07-09 09:42:55 -0300

    Update Comentario.java

--- src/comment/Comentario.java
+++ src/comment/Comentario.java
@@ -2,33 +2,40 @@ package comment;
 public class Comentario{
     private String conteudo;
     private String autor;
-    private Int idAutor;
+    private int idAutor;
     private String data;
 
-        public String getConteudo(){
-            return conteudo;
-        }
-        public void setConteudo(String conteudo){
+            public comentario(String conteudo, String autor, int idAutor, String data){
+            super();
             this.conteudo = conteudo;
-        }
-        public String getAutor(){
-            return autor;
-        }
-        public void setAutor(String autor){
             this.autor = autor;
-        }
-        public Int getIdAutor(){
-            return idAutor;
-        }
-        public void setIdAutor(Int idAutor){
             this.idAutor = idAutor;
-        }
-        public String getData(){
-            return data;
-        }
-        public void setData(String data){
             this.data = data;
-        }
+            }
+            public String getConteudo(){
+                return conteudo;
+            }
+            public void setConteudo(String conteudo){
+                this.conteudo = conteudo;
+            }
+            public String getAutor(){
+                return autor;
+            }
+            public void setAutor(String autor){
+                this.autor = autor;
+            }
+            public Int getIdAutor(){
+                return idAutor;
+            }
+            public void setIdAutor(Int idAutor){
+                this.idAutor = idAutor;
+            }
+            public String getData(){
+                return data;
+            }
+            public void setData(String data){
+                this.data = data;
+            }
 
 
 }
