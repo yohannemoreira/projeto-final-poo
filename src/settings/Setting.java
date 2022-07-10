9d84c76f3d338e704c3c6f71a860c999a43a2c83
Bruna <brunastefanie@alu.ufc.br>
2022-07-10 14:22:07 -0300

    Update Post.java

--- src/settings/Setting.java
+++ src/settings/Setting.java
@@ -1,73 +1,42 @@
-	package Settings;
+package Settings;
 
 import package account;
-
 public class Settings {
-	public String personalInformation;
-	public String accountStatus;
-	public String accountManagement;
-	publilc Account account;
 
+	public Account account;
 
-	public Settings(String personalInformation, String accountStatus, String activityStatus, String accountManagement) {
-		super();
-		this.personalInformation = personalInformation;
-		this.accountStatus = accountStatus;
-		this.accountManagement = accountManagement;
-	}
-
-	public String getPersonalInformation() {
-		return personalInformation;
-	}
-
-	public void setPersonalInformation(String personalInformation) {
-		this.personalInformation = personalInformation;
-	}
-
-	public String getAccountStatus() {
-		return accountStatus;
-	}
-
-	public void setAccountStatus(String accountStatus) {
-		this.accountStatus = accountStatus;
-	}
-
-	public String getAccountManagement() {
-		return accountManagement;
-	}
-
-	public void setAccountManagement(String accountManagement) {
-		this.accountManagement = accountManagement;
+	//To see personal informations from the account
+	private String seePersonalInformation(){
+		System.out.println("Name: " + account.getName() + "/n" + "User: " + account.getUser() + "/n" + "E-mail: " + account.getEmail() + 
+				"/n" + "Course: " + account.getCourse() + "/n"  +"Semester: " + account.getSemester() );
 	}
 	
-	
-private String seePersonalInformation(String personalInformation) {
-	
-}
+	//To see account status
+	private String seeAccountStatus(String accountStatus) {
+		System.out.println("Number of Followers " + followers.size() + "/n");
+		System.out.println("Number of Posts " + posts.size() + "/n");
+		System.out.println("Number of Exams " + exams.size() + "/n");
+	}	
 
-private String seeAccountStatus(String accountStatus) {
-	
-}	
-
-private void changePassword (String newPassword) {
-	account.findAccount(account.setPassword(password)).changePassword(password);
-}
-
-private void changeName(String newName) {
-	account.findAccount(account.setName(name)).changeName(name);
+	private void changePassword (String newPassword) {
+		account.findAccount(account.setPassword(password)).changePassword(password);
 	}
 
-private void changeEmail(String newEmail) {
-	account.findAccount(account.setEmail(email)).changeEmail(email);
-}
+	private void changeName(String newName) {
+		account.findAccount(account.setName(name)).changeName(name);
+	}
 
-private void logOut() {
-	
-}
+	private void changeEmail(String newEmail) {
+		account.findAccount(account.setEmail(email)).changeEmail(email);
+	}
 
-private void deleteAccount() {
+	private void logOut() {
 	
-}
+	}
+
+	private void deleteAccount() {
+	
+	}
 	
 }
 
