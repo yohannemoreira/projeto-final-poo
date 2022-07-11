9b2b473041575f4999272145cf90b2bda017d6f0
cmatheuslf <88686399+cmatheuslf@users.noreply.github.com>
2022-07-10 21:25:56 -0300

    Update mudança de nome

--- src/post/PostRepository.java
+++ src/post/PostRepository.java
@@ -2,24 +2,34 @@ package post;
 
 import java.util.ArrayList;
 
-public class RepositorioPostArray implements IRepositorio{
-	ArrayList<Post> repositorio;
-	
+public class PostCollection implements IPostCollection{
+	ArrayList<Post> PostCollection;
 	public void addPost(Post post) {
-		
+		PostCollection.add(post);
 	}
-	public void removePost(int indice) {
-		
+	public void removePost(int id) {
+		int indice = -1;
+		if(PostCollection.size()>0) {
+			for(int i = 0; i < PostCollection.size();i++) {
+				if(PostCollection.get(i).getId() == id) {
+					indice = i;
+				}
+			}
+			PostCollection.remove(PostCollection.get(indice));
+		}
+		else if(PostCollection.size()<=0) {
+			System.out.println("Sem posts");
+		}
 	}
 	public void updatePost(Post post) {
-		
+		//fazer
 	}
-	public Post searchPost(String id) {
+	public Post searchPost(int id) {
 		Post procurado = null;
 		
 		return procurado;
 	}
-	public boolean exist(String id) {
+	public boolean exist(int id) {
 		return true;
 	}
 }
