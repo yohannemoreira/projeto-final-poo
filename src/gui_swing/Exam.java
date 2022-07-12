508ffea320f1a288be747bff12d91dbbb12b66da
cmatheuslf <88686399+cmatheuslf@users.noreply.github.com>
2022-07-12 11:40:15 -0300

    upload exam

---
+++ src/gui_swing/Exam.java
@@ -0,0 +1,201 @@
+package telas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.GridBagLayout;
+import java.awt.CardLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+
+public class Exam {
+
+	private JFrame frmAvaliao;
+	private JTextField textField;
+	private JTextField textField_2;
+	private JTextField textField_1;
+	private JTextField textField_3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Exam window = new Exam();
+					window.frmAvaliao.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Exam() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmAvaliao = new JFrame();
+		frmAvaliao.setAutoRequestFocus(false);
+		frmAvaliao.getContentPane().setBackground(Color.DARK_GRAY);
+		frmAvaliao.setBackground(Color.DARK_GRAY);
+		frmAvaliao.setTitle("Avaliação");
+		frmAvaliao.setBounds(100, 100, 600, 600);
+		frmAvaliao.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		frmAvaliao.getContentPane().setLayout(gridBagLayout);
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 2;
+		gbc_lblNewLabel_1.gridy = 2;
+		GridBagConstraints gbc_lblNewLabel1 = new GridBagConstraints();
+		gbc_lblNewLabel1.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel1.gridx = 2;
+		gbc_lblNewLabel1.gridy = 3;
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.DARK_GRAY);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(50, 50, 5, 5);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 1;
+		gbc_panel.gridy = 1;
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{177, 74, 96, 0};
+		gbl_panel.rowHeights = new int[]{52, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		
+		JLabel lblNewLabel = new JLabel("Avaliação");
+		lblNewLabel.setForeground(Color.YELLOW);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 43));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.NORTHWEST;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		panel.add(lblNewLabel, gbc_lblNewLabel);
+		
+		frmAvaliao.getContentPane().add(panel, gbc_panel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Atividade");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_1.setForeground(Color.YELLOW);
+		GridBagConstraints gbc_lblNewLabel_11 = new GridBagConstraints();
+		gbc_lblNewLabel_11.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_11.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_11.gridx = 0;
+		gbc_lblNewLabel_11.gridy = 1;
+		panel.add(lblNewLabel_1, gbc_lblNewLabel_11);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_1.setForeground(Color.YELLOW);
+		
+		textField_3 = new JTextField();
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 0;
+		gbc_textField_3.gridy = 2;
+		panel.add(textField_3, gbc_textField_3);
+		textField_3.setColumns(10);
+		
+				
+		JLabel lblNewLabel_3 = new JLabel("Data");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 0;
+		gbc_lblNewLabel_3.gridy = 3;
+		panel.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_3.setForeground(Color.YELLOW);
+				
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.ipadx = 50;
+		gbc_textField_1.anchor = GridBagConstraints.WEST;
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.gridx = 0;
+		gbc_textField_1.gridy = 5;
+		panel.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Descrição	");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 6;
+		panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_2.setForeground(Color.YELLOW);
+		
+		textField_2 = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.ipady = 50;
+		gbc_textField_2.ipadx = 50;
+		gbc_textField_2.insets = new Insets(0, 0, 0, 5);
+		gbc_textField_2.anchor = GridBagConstraints.WEST;
+		gbc_textField_2.gridx = 0;
+		gbc_textField_2.gridy = 7;
+		panel.add(textField_2, gbc_textField_2);
+		textField_2.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Cadastrar");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridx = 2;
+		gbc_btnNewButton.gridy = 7;
+		panel.add(btnNewButton, gbc_btnNewButton);
+		ImageIcon image = new ImageIcon(Exam.class.getResource("/image/group.png"));
+		
+		
+		
+		
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setForeground(Color.DARK_GRAY);
+		panel_1.setBackground(Color.DARK_GRAY);
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.insets = new Insets(150, 0, 5, 5);
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.gridx = 5;
+		gbc_panel_1.gridy = 1;
+		frmAvaliao.getContentPane().add(panel_1, gbc_panel_1);
+		
+		JLabel lblNewLabel_4 = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.insets = new Insets(50, 50, 5, 5);
+		gbc_lblNewLabel_4.gridx = 5;
+		gbc_lblNewLabel_4.gridy = 1;
+		panel_1.add(lblNewLabel_4, gbc_lblNewLabel_4);
+		lblNewLabel_4.setIcon(new ImageIcon(image.getImage().getScaledInstance(130, 120, Image.SCALE_DEFAULT)));
+	}
+
+}
