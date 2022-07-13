c4fede484b7fe47de3d2df8b0148370b677acd90
cmatheuslf <88686399+cmatheuslf@users.noreply.github.com>
2022-07-12 22:37:13 -0300

    Versão melhor

--- src/gui_swing/Settings.java
+++ src/gui_swing/Settings.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.Color;
 import java.awt.GridBagLayout;
+import java.awt.Image;
+
 import javax.swing.JPanel;
 import java.awt.GridBagConstraints;
 import javax.swing.JLabel;
@@ -12,6 +14,11 @@ import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.Insets;
 import java.awt.Button;
+import java.awt.SystemColor;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.ImageIcon;
+import java.awt.FlowLayout;
 
 public class Settings {
 
@@ -45,19 +52,22 @@ public class Settings {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setAlwaysOnTop(true);
+		frame.setBackground(Color.LIGHT_GRAY);
 		frame.getContentPane().setBackground(Color.DARK_GRAY);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{4, 0, 378, 0};
+
+		frame.setBounds(100, 100, 600, 400);
+		gridBagLayout.columnWidths = new int[]{4, 0, 0, 0, 0, 378, 0};
 		gridBagLayout.rowHeights = new int[]{213, 0, 45, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
 		frame.getContentPane().setLayout(gridBagLayout);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.DARK_GRAY);
 		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.fill = GridBagConstraints.BOTH;
-		gbc_panel.insets = new Insets(0, 0, 5, 5);
+		gbc_panel.insets = new Insets(0, 170, 0, 0);
 		gbc_panel.gridx = 0;
 		gbc_panel.gridy = 0;
 		frame.getContentPane().add(panel, gbc_panel);
@@ -73,78 +83,87 @@ public class Settings {
 		tSettings.setForeground(Color.YELLOW);
 		tSettings.setBackground(new Color(255, 255, 0));
 		GridBagConstraints gbc_tSettings = new GridBagConstraints();
+		gbc_tSettings.gridwidth = 2;
 		gbc_tSettings.insets = new Insets(0, 0, 5, 5);
 		gbc_tSettings.gridx = 0;
 		gbc_tSettings.gridy = 0;
 		panel.add(tSettings, gbc_tSettings);
 		
-		JButton editProfile = new JButton("Editar Perfil");
-		editProfile.setBackground(new Color(0,0,0,0));
-		editProfile.setForeground(Color.YELLOW);
-		GridBagConstraints gbc_editProfile = new GridBagConstraints();
-		gbc_editProfile.ipadx = 15;
-		gbc_editProfile.insets = new Insets(0, 0, 5, 5);
-		gbc_editProfile.anchor = GridBagConstraints.WEST;
-		gbc_editProfile.gridx = 0;
-		gbc_editProfile.gridy = 2;
-		panel.add(editProfile, gbc_editProfile);
+		JButton editAccount = new JButton("Editar conta");
+		editAccount.setBackground(Color.DARK_GRAY);
+		editAccount.setForeground(Color.YELLOW);
+		GridBagConstraints gbc_editAccount = new GridBagConstraints();
+		gbc_editAccount.fill = GridBagConstraints.HORIZONTAL;
+		gbc_editAccount.anchor = GridBagConstraints.WEST;
+		gbc_editAccount.insets = new Insets(0, 0, 5, 5);
+		gbc_editAccount.gridx = 0;
+		gbc_editAccount.gridy = 1;
+		panel.add(editAccount, gbc_editAccount);
 		
 		JButton status = new JButton("Ver status");
-		status.setBackground(new Color(0,0,0,0));
 		status.setForeground(Color.YELLOW);
+		status.setBackground(Color.DARK_GRAY);
 		GridBagConstraints gbc_status = new GridBagConstraints();
-		gbc_status.ipadx = 22;
+		gbc_status.fill = GridBagConstraints.HORIZONTAL;
 		gbc_status.insets = new Insets(0, 0, 5, 5);
-		gbc_status.anchor = GridBagConstraints.WEST;
 		gbc_status.gridx = 0;
-		gbc_status.gridy = 3;
+		gbc_status.gridy = 2;
 		panel.add(status, gbc_status);
 		
 		JButton removeAccount = new JButton("Excluir conta");
-		removeAccount.setBackground(new Color(0,0,0,0));
 		removeAccount.setForeground(Color.YELLOW);
+		removeAccount.setBackground(Color.DARK_GRAY);
 		GridBagConstraints gbc_removeAccount = new GridBagConstraints();
-		gbc_removeAccount.ipadx = 10;
+		gbc_removeAccount.fill = GridBagConstraints.HORIZONTAL;
 		gbc_removeAccount.insets = new Insets(0, 0, 5, 5);
-		gbc_removeAccount.anchor = GridBagConstraints.WEST;
 		gbc_removeAccount.gridx = 0;
-		gbc_removeAccount.gridy = 4;
+		gbc_removeAccount.gridy = 3;
 		panel.add(removeAccount, gbc_removeAccount);
 		
 		JButton back = new JButton("Voltar");
-		removeAccount.setBackground(new Color(0,0,0,0));
 		back.setForeground(Color.YELLOW);
+		back.setBackground(Color.DARK_GRAY);
 		GridBagConstraints gbc_back = new GridBagConstraints();
-		gbc_back.ipadx = 40;
+		gbc_back.fill = GridBagConstraints.HORIZONTAL;
 		gbc_back.insets = new Insets(0, 0, 5, 5);
-		gbc_back.anchor = GridBagConstraints.WEST;
 		gbc_back.gridx = 0;
-		gbc_back.gridy = 5;
+		gbc_back.gridy = 4;
 		panel.add(back, gbc_back);
 		
 		JButton exit = new JButton("Sair");
-		exit.setBackground(new Color(0,0,0,0));
 		exit.setForeground(Color.YELLOW);
+		exit.setBackground(Color.DARK_GRAY);
 		GridBagConstraints gbc_exit = new GridBagConstraints();
-		gbc_exit.insets = new Insets(0, 0, 0, 5);
+		gbc_exit.fill = GridBagConstraints.HORIZONTAL;
 		gbc_exit.ipadx = 50;
-		gbc_exit.anchor = GridBagConstraints.WEST;
+		gbc_exit.insets = new Insets(0, 0, 5, 5);
 		gbc_exit.gridx = 0;
-		gbc_exit.gridy = 6;
+		gbc_exit.gridy = 5;
 		panel.add(exit, gbc_exit);
 		
+		Component horizontalStrut = Box.createHorizontalStrut(20);
+		GridBagConstraints gbc_horizontalStrut = new GridBagConstraints();
+		gbc_horizontalStrut.insets = new Insets(0, 0, 5, 5);
+		gbc_horizontalStrut.gridx = 1;
+		gbc_horizontalStrut.gridy = 0;
+		frame.getContentPane().add(horizontalStrut, gbc_horizontalStrut);
+		
 		JPanel panel_1 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
+		panel_1.setBackground(Color.DARK_GRAY);
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
 		gbc_panel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_panel_1.fill = GridBagConstraints.BOTH;
-		gbc_panel_1.gridx = 1;
+		gbc_panel_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel_1.gridx = 2;
 		gbc_panel_1.gridy = 0;
 		frame.getContentPane().add(panel_1, gbc_panel_1);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		panel_1.add(lblNewLabel);
-		frame.setBounds(100, 100, 400, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel settings = new JLabel("");
+		ImageIcon image = new ImageIcon(Exam.class.getResource("/image/group.png"));
+		settings.setIcon(new ImageIcon(image.getImage().getScaledInstance(130, 120, Image.SCALE_DEFAULT)));
+		panel_1.add(settings);
+		
+				
 	}
 
 }
