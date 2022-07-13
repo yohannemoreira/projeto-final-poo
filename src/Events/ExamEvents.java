1771173c760794817e0f325a5ff40b51bd4617c6
Bruna <brunastefanie@alu.ufc.br>
2022-07-13 13:35:33 -0300

    Update ExamEvents.java

--- src/Events/ExamEvents.java
+++ src/Events/ExamEvents.java
@@ -2,17 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/**
+ * Classe ExamEvents.
+ *
+ * @author Bruna e Gabriela.
+ * @version 2.0
+ * <br>
+ * Copyright (C) 2022 Universidade Federal do
+Ceará.
+ */
 package Events;
 
 import account.Account;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-/**
- *
- * @author Yohanne
- */
 public class ExamEvents {
+    // atributos
     private String nameExam;
     private String dateExam;
     private String description;
