4df08cdf584d3d33e585cdcba93fc5a48eea4dcd
gabiponciano <105561496+gabiponciano@users.noreply.github.com>
2022-07-14 11:22:44 -0300

    Update AccountRegistration.java

--- src/account/AccountRegistration.java
+++ src/account/AccountRegistration.java
@@ -1,3 +1,12 @@
+/**
+ * Classe Account Registration.
+ *
+ * @author Yohanne Wildnei
+ * @version 1.0
+ * <br>
+ * Copyright (C) 2022 Universidade Federal do
+Ceará.
+ */
 package account;
 
 import java.util.Collection;
@@ -7,14 +16,33 @@ import account.exceptions.NotFoundException;
 import account.exceptions.RegisteredAccountException;
 
 interface AccountRegistration {
-
+/**
+* Adiciona uma conta.
+* @param account Objeto do tipo conta.
+* @exception RegisteredAccountException Exceção que ocorre quando uma conta já é registrada.
+* @exception ExistingUserException Exceção que ocorre quando o usuario já existe.
+*/
 	public void addAccount(Account account) throws RegisteredAccountException, ExistingUserException ;
-
+/**
+* Remove uma conta.
+* @param account Objeto do tipo de conta.
+* @exception NotfoundException Exceção que ocorre quando o usuario não é encontrado.
+*/
 	public void removeAccount(Account account) throws NotFoundException;
-	
+/**
+* Procura uma conta.
+* @param email Email da conta.
+* @exception NotfoundException Exceção que ocorre quando o email não é encontrado.
+*/	
 	public Account findAccount (String email) throws NotFoundException;
-	
+/**
+* Procura um usúario.
+* @param user Usúario da conta.
+* @exception ExistingUserException Exceção que ocorre quando o usúario já existe.
+*/
 	public Account searchUser(String user) throws ExistingUserException;
-
+/**
+* Faz uma lista da coleção de contas.
+*/
 	public Collection<Account> list();
 }
