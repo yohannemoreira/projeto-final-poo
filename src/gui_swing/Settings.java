95c78c1a3695c26aab1da41d1bd2a1cdd631e902
gabiponciano <105561496+gabiponciano@users.noreply.github.com>
2022-07-14 12:54:14 -0300

    Update Settings.java

--- src/gui_swing/Settings.java
+++ src/gui_swing/Settings.java
@@ -1,3 +1,12 @@
+/**
+ * Classe Settings.
+ *
+ * @author Yohanne e Gabriela.
+ * @version 2.0
+ * <br>
+ * Copyright (C) 2022 Universidade Federal do
+Ceará.
+ */
 package gui_swing;
 
 import java.awt.EventQueue;
