416ff927d0997e1e9469ad4501a19cf67f80a653
gabiponciano <105561496+gabiponciano@users.noreply.github.com>
2022-07-14 11:26:25 -0300

    Update Follower.java

--- src/account/Follower.java
+++ src/account/Follower.java
@@ -1,23 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * Classe Follower.
+ *
+ * @author Yohanne e Wildnei
+ * @version 2.0
+ * <br>
+ * Copyright (C) 2022 Universidade Federal do
+Ceará.
  */
 package account;
 
-/**
- *
- * @author Yohanne
- */
 class Follower {
-
+// atributos
     private Account follow;
     private Account follower;
-
+/**
+* Cria um Seguidor usando a "seguida" da conta e o seguidor da conta.
+* @param follow "Seguida" da conta.
+* @param follower Seguidor da conta.
+*/
     public Follower(Account follow, Account follower) {
 	this.follow = follow;
 	this.follower = follower;
     }
-
+/**
+* Recupera a seguida.
+* @return A seguida.
+*/
     public Account getFollow() {
 	return follow;
     }
@@ -25,7 +33,10 @@ class Follower {
     public void setFollow(Account follow) {
 	this.follow = follow;
     }
-
+/**
+* Recupera o seguidor.
+* @return O seguidor.
+*/
     public Account getFollower() {
 	return follower;
     }
