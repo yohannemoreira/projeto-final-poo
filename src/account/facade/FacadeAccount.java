9e4cf5a5b4f26c7ed9c37b4f208636e5b35fe672
gabiponciano <105561496+gabiponciano@users.noreply.github.com>
2022-07-14 11:02:16 -0300

    Update FacadeAccount.java

--- src/account/facade/FacadeAccount.java
+++ src/account/facade/FacadeAccount.java
@@ -1,6 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/** @author Yohanne
+ * @version 2.0
+ * <br>
+ * Copyright (C) 2022 Universidade Federal do
+Ceará.
  */
 package account.facade;
 
@@ -27,23 +29,42 @@ public class FacadeAccount {
     private FacadeAccount(){
 	cntrlAccount = new AccountController();
     }
-
+/**
+* Recupera as contas.
+* @return As contas.
+*/
    public AccountCollection getAccounts() {
 	return cntrlAccount.getAccounts();
     }
-
+/**
+* Adiciona uma conta.
+* @param account Objeto do tipo de conta.
+* @exception RegisteredAccountException Exceção que ocorre quando uma conta já é registrada.
+* @exception ExistingUserException Exceção que ocorre quando o usuario já existe.
+*/
     public void addAccount(Account account) throws RegisteredAccountException, ExistingUserException{
 	cntrlAccount.addAccount(account);
     }
-
+/**
+* Remove uma conta.
+* @param account Objeto do tipo de conta.
+* @exception NotfoundException Exceção que ocorre quando o usuario não é encontrado.
+*/
     public void removeAccount(Account account) throws NotFoundException {
 	cntrlAccount.removeAccount(account);
     }
-
+/**
+* Procura uma conta.
+* @param email Email da conta.
+* @exception NotfoundException Exceção que ocorre quando o email não é encontrado.
+*/
     public Account findAccount(String email) throws NotFoundException {
 	return cntrlAccount.findAccount(email);
     }
-
+ /**
+* Faz uma lista da coleção de conta.
+* @return lista de conta.
+*/
     public Collection<Account> list() {
 	return cntrlAccount.list();
 
