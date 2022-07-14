f5119d8202bc2e4ae0df878ccc74ba090de23e57
gabiponciano <105561496+gabiponciano@users.noreply.github.com>
2022-07-14 10:44:20 -0300

    Update PostCollection.java

--- src/post/PostCollection.java
+++ src/post/PostCollection.java
@@ -1,3 +1,13 @@
+/**
+ * Classe Post Collection.
+ *
+ * @author Carlos Matheus
+ * @version 3.0
+ * <br>
+ * Copyright (C) 2022 Universidade Federal do
+Ceará.
+ */
+
 package post;
 
 import java.util.ArrayList;
@@ -11,14 +21,17 @@ public class PostCollection implements IPostCollection{
 	public void addPost(Post post) {
 		postCollection.add(post);
 	}
-	    /**
-* Recupera uma coleção de posr.
+/**
+* Recupera uma coleção de post.
 * @return A coleção de post.
 */
 	public ArrayList<Post> getPostCollection(){
 		return postCollection;
 	}
-	
+/**
+* Remove um Post.
+* @param id Identificação do post .
+*/	
 	public void removePost(int id) {
 		int indice = -1;
 		if(postCollection.size()>0) {
@@ -33,17 +46,34 @@ public class PostCollection implements IPostCollection{
 			System.out.println("Sem posts");
 		}
 	}
+/**
+* Atualiza um Post.
+* @param post Objeto do tipo post .
+*/
 	public void updatePost(Post post) {
 		//fazer
 	}
+/**
+* Procura um Post.
+* @param id Identificação do post.
+*/
 	public Post searchPost(int id) {
 		Post procurado = null;
 		
 		return procurado;
 	}
+/**
+ * Vê se um Post existe.
+* @param id Identificação de um post.
+* @return Se é verdadeiro.
+*/
 	public boolean exist(int id) {
 		return true;
 	}
+/**
+* Vê o tamanho da coleção.
+* @return O tamanho da coleção.
+*/
 	public int sizeArray () {
 		return postCollection.size();
 	}
